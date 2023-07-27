fcc77be2adbb80ece53177851f350f7c832222c0
Sercan ISLI <sercanislii@gmail.com>
2023-07-28 02:13:55 +0300

    Add project files.

---
+++ BookStoreWebApi/Controllers/BooksController.cs
@@ -0,0 +1,143 @@
+﻿using BookStoreWebApi.Models;
+using BookStoreWebApi.Repositories.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            var book = _context.Books.Where(b => b.Id == id).SingleOrDefault();
+            try
+            {
+                if (book == null)
+                {
+                    return NotFound();
+                }
+                return Ok(book);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult CreareOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if (book == null)
+                {
+                    return BadRequest(); //400
+                }
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id == id).SingleOrDefault();
+                if (entity == null)
+                {
+                    return NotFound();
+                }
+                //id check
+                if (id != book.Id)
+                {
+                    return BadRequest();
+                }
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+                _context.SaveChanges();
+                return StatusCode(200, book);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            try
+            {
+                var entity = _context.Books.Where(b => b.Id == id).SingleOrDefault();
+                if (entity == null)
+                {
+                    return NotFound(new
+                    {
+                        message = $"Book with id:{id} could not found",
+                        statusCode = 404
+                    });
+                }
+                _context.Books.Remove(entity);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        [HttpPatch(" {id:int}")]
+        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Book> bookPatch)
+        {
+            try
+            {
+                var entitiy = _context.Books.Where(b => b.Id == id).SingleOrDefault();
+                if (entitiy == null)
+                {
+                    return NotFound();
+                }
+                bookPatch.ApplyTo(entitiy);
+                _context.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ BookStoreWebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,25 @@
+﻿using BookStoreWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookStoreWebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Karagöz ve Hacivat", Price = 75 },
+                new Book { Id = 2, Title = "Hamlet", Price = 5 },
+                new Book { Id = 3, Title = "Mesnevi", Price = 100 },
+                new Book { Id = 4, Title = "Araba Sevdası", Price = 15 },
+                new Book { Id = 5, Title = "Kaşağı", Price = 55 },
+                new Book { Id = 6, Title = "Uçurtma Avcısı", Price = 150 },
+                new Book { Id = 7, Title = "Ateşten Gömlek", Price = 100 },
+                new Book { Id = 8, Title = "Çalukuşu", Price = 90 },
+                new Book { Id = 9, Title = "Tutunamayanlar", Price = 45 },
+                new Book { Id = 10, Title = "Nutuk", Price = 10000 }
+            );
+        }
+    }
+}
