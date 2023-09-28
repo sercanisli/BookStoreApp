9a58091b1e3460be653a93356ceb978722465a9a
Sercan ISLI <sercanislii@gmail.com>
2023-09-28 22:31:21 +0300

    Repository Manager Created. Repository process are done.

---
+++ Repositories/EntityFrameworkCore/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using Repositories.Contracts;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateOneBook(Book book) => Create(book);
+
+        public void DeleteOneBook(Book book) => Delete(book);
+
+        public IQueryable<Book> GetAllBooks(bool trackChanges) => FindAll(trackChanges);
+
+        public IQueryable<Book> GetneBookById(int id, bool trackChanges) => FindByCondition(b => b.Id == id, trackChanges).OrderBy(b=>b.Id);
+
+        public void UpdateOneBook(Book book) => Update(book);
+    }
+}
