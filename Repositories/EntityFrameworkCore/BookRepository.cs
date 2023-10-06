43007ce0cb837ac11177798869c1090c42057008
Sercan ISLI <sercanislii@gmail.com>
2023-10-07 02:28:26 +0300

    Pagination structure has been implemented.

--- Repositories/EntityFrameworkCore/BookRepository.cs
+++ Repositories/EntityFrameworkCore/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
 
@@ -14,7 +15,14 @@ namespace Repositories.EntityFrameworkCore
 
         public void DeleteOneBook(Book book) => Delete(book);
 
-        public async Task<IEnumerable<Book>> GetAllBooksAsync(bool trackChanges) => await FindAll(trackChanges).OrderBy(b=>b.Id).ToListAsync();
+        public async Task<PagedList<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
+        {
+            var books = await FindAll(trackChanges)
+                .OrderBy(b => b.Id)
+                .ToListAsync();
+
+            return PagedList<Book>.ToPagedList(books, bookParameters.PageNumber, bookParameters.PageSize);
+        }
 
         public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges) => await FindByCondition(b => b.Id == id, trackChanges).OrderBy(b=>b.Id).SingleOrDefaultAsync();
 
