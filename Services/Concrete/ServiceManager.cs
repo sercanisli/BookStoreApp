486c6ba19477ef83cacd6ddab9abd41b8d006621
Sercan ISLI <sercanislii@gmail.com>
2023-10-20 18:05:57 +0300

    Service Manager definitions were made for Category.

--- Services/Concrete/ServiceManager.cs
+++ Services/Concrete/ServiceManager.cs
@@ -10,6 +10,7 @@ namespace Services.Concrete
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
+        private readonly Lazy<ICategoryService> _categoryService;
         private readonly Lazy<IAuthenticationService> _authenticationService;
         public ServiceManager(IRepositoryManager repositoryManager,
             ILoggerService logger,
@@ -21,10 +22,14 @@ namespace Services.Concrete
             _bookService = new Lazy<IBookService>(() => 
                 new BookManager(repositoryManager,logger,mapper, bookLinks));
 
+            _categoryService =new Lazy<ICategoryService>(() =>
+                new CategoryManager(mapper, logger, repositoryManager));
+
             _authenticationService = new Lazy<IAuthenticationService>(() =>
                 new AuthenticationManager(logger, mapper, userManager, configuration));
         }
         public IBookService BookService => _bookService.Value;
         public IAuthenticationService AuthenticationService => _authenticationService.Value;
+        public ICategoryService CategoryService => _categoryService.Value;
     }
 }
