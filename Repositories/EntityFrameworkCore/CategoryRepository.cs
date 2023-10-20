00c5a534c5a9667aefedda51e450fabe7553fcf3
Sercan ISLI <sercanislii@gmail.com>
2023-10-20 16:47:51 +0300

    Category entity has been created. Repository definitions were made for Category.

---
+++ Repositories/EntityFrameworkCore/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+
+namespace Repositories.EntityFrameworkCore
+{
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(RepositoryContext context) : base(context)
+        {
+        }
+
+        public void CreateOneCategory(Category category) => Create(category);
+
+        public void DeleteOneCategory(Category category) =>Delete(category);
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync(bool trackChanges) =>
+            await FindAll(trackChanges).OrderBy(c => c.CategoryName).ToListAsync();
+
+        public async Task<Category>? GetOneCategoryByIdAsync(int id, bool trackChanges) =>
+            await FindByCondition(c => c.CategoryId == id, trackChanges).SingleOrDefaultAsync();
+
+        public void UpdateOneCategory(Category category) => Update(category);
+    }
+}
--- Repositories/EntityFrameworkCore/RepositoryContext.cs
+++ Repositories/EntityFrameworkCore/RepositoryContext.cs
@@ -14,6 +14,7 @@ namespace Repositories.EntityFrameworkCore
         }
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
